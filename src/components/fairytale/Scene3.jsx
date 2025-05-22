a3cb24c7dc5b6a1b8418188d84cf8444fe52c106
PemaBhutia2005 <pema.bhutia@student.ehb.be>
2025-05-22 12:53:32 +0200

    Text shows on hover

--- src/components/fairytale/Scene3.jsx
+++ src/components/fairytale/Scene3.jsx
@@ -1,16 +1,28 @@
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import { useRef } from "react";
 
+import { useState } from "react";
+
 import portal from "../../assets/portal.png";
 import doorPortal from "../../assets/door-portal.png";
 
 export default function Scene3() {
+	const [hasHovered, setHasHovered] = useState(false);
+
+	const handleHover = () => {
+		if (!hasHovered) {
+			setHasHovered(true);
+		}
+	};
+
 	return (
 		<div className="scene-container">
 			<p className="scene-text scene3-text">In haar nieuwe huis ontdekt ze een mysterieuze deur.</p>
 
 			<img src={doorPortal} className="scene2-house door-portal" alt="Door portal" />
-			<img src={portal} className="portal"></img>
+			<img src={portal} className="portal" alt="Glowing portal" onMouseEnter={handleHover} />
+
+			{hasHovered && <p className="scene-text scene3-text2 portal-hover-text">Het is een portaal naar een andere wereld!</p>}
 		</div>
 	);
 }
