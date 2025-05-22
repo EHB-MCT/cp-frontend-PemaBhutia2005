620a8722d1ef66617dc4da97158fa3799e7f0bd1
PemaBhutia2005 <pema.bhutia@student.ehb.be>
2025-05-22 13:38:46 +0200

    Scene4 portraits scroll parallax

--- src/components/fairytale/Scene4.jsx
+++ src/components/fairytale/Scene4.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import { useRef } from "react";
 
 import portrait from "../../assets/portrait1.png";
 import portrait2 from "../../assets/portrait2.png";
 import cake from "../../assets/cake.png";
 
 export default function Scene4() {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ["start start", "end end"],
+	});
+
+	const smoothScroll = useSpring(scrollYProgress, {
+		stiffness: 100,
+		damping: 30,
+		mass: 1,
+	});
+
+	const portraitY = useTransform(smoothScroll, [0, 1], ["100%", "-10%"]);
+
+	const cakeY = useTransform(smoothScroll, [0.5, 1], ["30%", "0%"]);
+
 	return (
 		<div className="scene-container">
 			<p className="scene-text scene4-text">In deze andere wereld lijkt alles perfect.</p>
 
 			<div className="scene4-portraits">
-				<img src={portrait} alt="Portrait" className="scene4-portrait" />
-				<img src={cake} alt="Cake" className="scene4-portrait" />
-				<img src={portrait2} alt="Portrait" className="scene4-portrait" />
+				<motion.img src={portrait} alt="Portrait" className="scene4-portrait" style={{ y: portraitY }} />
+				<motion.img src={cake} alt="Cake" className="scene4-portrait cake" style={{ y: cakeY }} />
+				<motion.img src={portrait2} alt="Portrait" className="scene4-portrait" style={{ y: portraitY }} />
 			</div>
 		</div>
 	);
