2e80ad256a461cfdfd01cf7b75c4533443a28bd3
PemaBhutia2005 <pema.bhutia@student.ehb.be>
2025-05-24 18:15:53 +0200

    Fetch fairytales from JSON

--- src/components/portal/ProjectGrid.jsx
+++ src/components/portal/ProjectGrid.jsx
@@ -1,39 +1,12 @@
+// ProjectsGrid.jsx
 import ProjectCard from "./ProjectCard";
 
-import roodkapje from "../../assets/roodkapje.jpg";
-import sneeuwwitje from "../../assets/sneeuwwitje.png";
-import wolfGeitjes from "../../assets/wolfGeitjes.png";
-import hansGrietje from "../../assets/hansGrietje.jpg";
-
-const projects = [
-	{
-		title: "ROODKAPJE",
-		author: "Fleur Van De Ville",
-		imgSrc: roodkapje,
-	},
-	{
-		title: "HANS EN GRIETJE",
-		author: "Arno Baeck",
-		imgSrc: hansGrietje,
-	},
-	{
-		title: "DE WOLF EN DE 7 GEITJES",
-		author: "Sam Hoeterickx",
-		imgSrc: wolfGeitjes,
-	},
-	{
-		title: "SNEEUWWITJE",
-		author: "Matteo Giamberresi",
-		imgSrc: sneeuwwitje,
-	},
-];
-
-export default function ProjectsGrid() {
+export default function ProjectsGrid({ projects = [] }) {
 	return (
 		<div className="projects-wrapper">
 			<div className="projects-grid">
 				{projects.map((project, index) => (
-					<ProjectCard key={index} {...project} />
+					<ProjectCard key={index} title={project.fairytale} author={project.nameStudent} imgSrc={project.imgThumbnail} />
 				))}
 			</div>
 			<div className="center">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 import ProjectsGrid from "../components/portal/ProjectGrid";
 import Footer from "../components/portal/Footer";
 
 function Home() {
-	const [count, setCount] = useState(0);
+	const [fairytales, setFairytales] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch("https://raw.githubusercontent.com/EHB-MCT/cp-frontend-MaximWesterbeek/refs/heads/main/course-project/public/api/fairytaleList.json")
+			.then((res) => res.json())
+			.then((data) => setFairytales(data))
+			.catch((err) => {
+				console.error("Failed to load fairytales", err);
+				setError(err.message);
+			});
+	}, []);
+
+	if (error) return <p>Error loading data: {error}</p>;
+	if (!fairytales) return <p>Loading...</p>;
 
 	return (
-		<>
-			<main>
-				<h1 className="bungee-regular">In the spotlight</h1>
-				<ProjectsGrid />
-				<Footer />
-			</main>
-		</>
+		<main>
+			<h1 className="bungee-regular">In the spotlight</h1>
+			<ProjectsGrid projects={fairytales} />
+			<Footer />
+		</main>
 	);
 }
 
